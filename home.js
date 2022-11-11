d9799ecd22c4086cb9ec8c7793028f0dbbe8676f
shehbaz khanani <cuteboyusman49@gmail.com>
2022-11-11 23:46:05 +0500

    update

--- home.js
+++ home.js
@@ -1,3 +1,28 @@
+//Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
+import { getAuth, signOut,
+    onAuthStateChanged, } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBnbjCGPgoR8oVKE110hqNI7U1_iqv4rPo",
+  authDomain: "fir-modulea-cc5b9.firebaseapp.com",
+  projectId: "fir-modulea-cc5b9",
+  storageBucket: "fir-modulea-cc5b9.appspot.com",
+  messagingSenderId: "569272878944",
+  appId: "1:569272878944:web:b1753d298862a86f820005",
+  measurementId: "G-C06VTDY3KQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
+
 let input = document.getElementById('input');
 let display = document.getElementById('parent');
 
@@ -11,10 +36,8 @@ function todo (e) {
     }
      
     arr.push(model)
-    console.log(model, 'modeel');
-    console.log(arr, 'arr');
-
     render()
+
     input.value = ""
 }
 
@@ -24,3 +47,31 @@ function render(){
         display.innerHTML += `<li> ${arr[i].todo} <br> <span> ${arr[i].time} </span> </li>`
     }
 }
+
+window.logout = function () {
+    signOut(auth)
+      .then(function () {
+        console.log("Logout Successfully");
+        window.location.href = "login.html";
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  };
+
+function checkAuthentication() {
+    onAuthStateChanged(auth, function (user) {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        console.log(uid);
+        // ...
+      } else {
+        // User is signed out
+        // ...
+        window.location.href = "login.html";
+      }
+    });
+  }
+  checkAuthentication();
--- login.js
+++ login.js
@@ -1,15 +1,43 @@
+//Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-analytics.js";
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBnbjCGPgoR8oVKE110hqNI7U1_iqv4rPo",
+  authDomain: "fir-modulea-cc5b9.firebaseapp.com",
+  projectId: "fir-modulea-cc5b9",
+  storageBucket: "fir-modulea-cc5b9.appspot.com",
+  messagingSenderId: "569272878944",
+  appId: "1:569272878944:web:b1753d298862a86f820005",
+  measurementId: "G-C06VTDY3KQ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth();
 let email = document.getElementById('email');
 let password = document.getElementById('password');
 
-let model = []
 
-function signup(e) {
+window.login = function(e) {
     e.preventDefault()
-    let arr = {
+    let model = {
         Email : email.value,
         Password : password.value,
     }
-    model.push(arr)
-    console.log(arr, 'arr');
-    console.log(model, 'model');
+
+    signInWithEmailAndPassword(auth, model.Email, model.Password).
+    then(function(succ){
+        console.log(succ.user.uid);
+        window.location.replace('home.html')
+    }).catch(function(err){
+        console.log(err);
+    })
+
 }
